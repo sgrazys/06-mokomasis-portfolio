1e9e50c3243369e96863ecc217d4a0f28e7e40c2
sgrazys <s.grazys@gmail.com>
2023-01-27 15:51:58 +0200

    add hobbie section

--- js/components/isValid/IsValid.js
+++ js/components/isValid/IsValid.js
@@ -7,18 +7,42 @@ class IsValid {
     }
 
     static nonEmtyArray(arr) {
-        return Array.isArray(arr) && arr.length === 0;
+        return Array.isArray(arr) && arr.length > 0;
+    }
+
+    static positiveInteger(num) {
+        if (!Number.isInteger(num) || num < 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    static stringSizeInRange(str, max = 20, min = 1) {
+        if (!IsValid.positiveInteger(max)) {
+            max = 20;
+        }
+        if (!IsValid.positiveInteger(min)) {
+            max = 1;
+        }
+        if (typeof str !== 'string' || str.length < min || str.length > max) {
+            return false;
+        }
+        return true;
     }
 
     static icon(str) {
-        if (typeof str !== 'string' || str.length > 15 || str === '') {
+        const maxLength = 12;
+
+        if (!IsValid.stringSizeInRange(str, maxLength)) {
             return false;
         }
         return true;
     }
 
     static text(str) {
-        if (typeof str !== 'string' || str.length > 15 || str === '') {
+        const maxLength = 15;
+        if (!IsValid.stringSizeInRange(str, maxLength)) {
             return false;
         }
         return true;
